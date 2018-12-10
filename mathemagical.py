094824097f3ccf6ec06429d15d995d6fcbdad1db
Novalinium <nova@noblejury.com>
2018-12-10 12:33:16 -0500

    SymPy path in math

--- mathemagical.py
+++ mathemagical.py
@@ -10,7 +10,7 @@ async def latex_render_function(message, client, args):
         # Unclear how to sandbox this well without a function whitelist :/
         # See service file for sandboxing info
         preview_file = io.BytesIO()
-        preview(renderstring, viewer="BytesIO", output="png", outputbuffer=preview_file)
+        sympy.preview(renderstring, viewer="BytesIO", output="png", outputbuffer=preview_file)
         await message.channel.send("`"+renderstring+"`", file=preview_file)
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
