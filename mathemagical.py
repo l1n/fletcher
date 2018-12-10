b5ef0dbeba623e70defcd6a6be5cf81359af1136
Novalinium <nova@noblejury.com>
2018-12-10 12:51:45 -0500

    Increase font size (Take 3)

--- mathemagical.py
+++ mathemagical.py
@@ -7,7 +7,7 @@ import tempfile
 async def latex_render_function(message, client, args):
     try:
         renderstring = "$$"+" ".join(args)+"$$"
-        preamble = r'\documentclass[14pt]{article}\usepackage{mathtools}\pagenumbering{gobble}\begin{document}'
+        preamble = r'\documentclass[14pt]{article}\usepackage{mathtools}\pagenumbering{gobble}\DeclareMathSizes{12}{20}{14}{10}\begin{document}'
         # Unclear how to sandbox this well without a function whitelist :/
         # See service file for sandboxing info
         preview_file = io.BytesIO()
