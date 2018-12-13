5222aae2781b1f00be072821cea319b1c21d034f
Novalinium <nova@noblejury.com>
2018-12-13 14:41:25 -0500

    Uwu better responses

--- swag.py
+++ swag.py
@@ -1,12 +1,20 @@
 import discord
+import random
 # Super Waifu Animeted Girlfriend
+
+uwu_responses = [
+        '*blush* For me?',
+        'Aww, thanks <3',
+        '*giggles*',
+        'No u :3'
+        ]
 
 async def uwu_function(message, client, args):
     try:
         if len(args) == 2 and type(args[1]) is discord.User and message.author.id == client.user.id:
             return await args[1].send("Stop it, you're making me blush </3")
         elif len(args) == 0:
-            return await message.channel.send('*blush* For me?')
+            return await message.channel.send(random.choice(uwu_responses))
     except Exception as e:
         return e
 
