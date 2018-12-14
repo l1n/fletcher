7360d2e5de0400b34c39efc5ca98501d8db5958f
Novalinium <nova@noblejury.com>
2018-12-14 00:26:50 -0500

    Private uwuing

--- swag.py
+++ swag.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 # Super Waifu Animated Girlfriend
 
 uwu_responses = {
-        'text': [
+        'public': [
             '*blush* For me?',
             'Aww, thanks ❤',
             '*giggles*',
@@ -16,6 +16,12 @@ uwu_responses = {
             '*ruffles your hair* You\'re a cutie ^_^',
             'Can I get your number? Mine\'s 429368441577930753~'
         ],
+        'private': [
+            'Stop it, you\'re making me blush </3',
+            'You\'re too kind ^_^',
+            'Thanksss~',
+            'uwu to you too <3'
+            ],
         'reaction': [
             '❤', '💛', '💚', '💙', '💜', '💕', '💓', '💗', '💖', '💘', '💘', '💝'
             ]
@@ -24,11 +30,11 @@ uwu_responses = {
 async def uwu_function(message, client, args):
     try:
         if len(args) == 2 and type(args[1]) is discord.User and message.author.id == client.user.id:
-            return await args[1].send("Stop it, you're making me blush </3")
+            return await args[1].send(random.choice(uwu_responses['private']))
         elif len(args) == 0 or 'fletch' in message.clean_content.lower() or message.content[0] == "!" or "good bot" in message.content.lower():
             if random.randint(0, 100) < 20:
                 await message.add_reaction(random.choice(uwu_responses['reaction']))
-            return await message.channel.send(random.choice(uwu_responses['text']))
+            return await message.channel.send(random.choice(uwu_responses['public']))
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         print("UWU[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
