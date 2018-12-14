fb9b61b9222b2c71a96f192eae1c967c08e99728
Novalinium <nova@noblejury.com>
2018-12-14 14:35:49 -0500

    Versioning API returns message

--- versionutils.py
+++ versionutils.py
@@ -9,7 +9,7 @@ class VersionInfo:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.repo = Repo(dir_path)
         assert not self.repo.bare
-    def latest_commit_log():
+    def latest_commit_log(self):
         head = self.repo.head       # the head points to the active branch/ref
         master = head.reference     # retrieve the reference the head points to
-        return master.log()[-1]
+        return master.log()[-1].message
