cf878f9e8d2225c9fcd1ce91b4b89e2d40f86acc
Novalinium <nova@noblejury.com>
2018-12-17 15:03:27 -0500

    Google Photos API Authorization flow complete

--- googlephotos.py
+++ googlephotos.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 
 def authorize_googlephotos_function(message=None, client=None, args=None):
     global config
+    global state
     flow = google_auth_oauthlib.flow.Flow.from_client_config(
             {'web': config['google-photos']},
             ['https://www.googleapis.com/auth/photoslibrary.readonly'])
@@ -18,17 +19,21 @@ def authorize_googlephotos_function(message=None, client=None, args=None):
             # Enable offline access so that you can refresh an access token without
             # re-prompting the user for permission. Recommended for web server apps.
             access_type='offline',
+            prompt='consent',
             # Enable incremental authorization. Recommended as a best practice.
             include_granted_scopes='true')
-    print(authorization_url)
+    print('Head to this URL, then use the `!photos_login` command with the resulting URL as the parameter: '+authorization_url)
     return authorization_url
 
 def login_googlephotos_function(message=None, client=None, args=None):
     global config
     global gphotos
+    global state
     flow = google_auth_oauthlib.flow.Flow.from_client_config(
             {'web': config['google-photos']},
-            ['https://www.googleapis.com/auth/photoslibrary.readonly'])
+            ['https://www.googleapis.com/auth/photoslibrary.readonly'],
+            state=state)
+    flow.redirect_uri = 'https://novalinium.com/breksta-oauth'
     flow.fetch_token(authorization_response=args[0])
     credentials = flow.credentials
     freeze = """
@@ -40,8 +45,8 @@ client_id = {}
 client_secret = {}
 scopes = {}
 """.format(credentials.token,credentials.refresh_token,credentials.token_uri,credentials.client_id,credentials.client_secret,credentials.scopes)
-    gphotos = build('photos.library', 'v1', credentials=credentials)
     print(freeze)
+    gphotos = build('photoslibrary', 'v1', credentials=credentials)
     return freeze
 
 def autoload(ch):
@@ -49,8 +54,6 @@ def autoload(ch):
     global gphotos
     # if gphotos is not None:
     #     return
-    if 'refresh_token' not in config['google-photos']:
-        return authorize_googlephotos_function()
     ch.add_command({
         'trigger': ['!photos_login'],
         'function': login_googlephotos_function,
@@ -60,4 +63,6 @@ def autoload(ch):
         'args_name': [],
         'description': 'Log onto Google Photos (Admin)'
         })
-    gphotos = build('photos.library', 'v1', credentials=config['google-photos'])
+    if 'refresh_token' not in config['google-photos']:
+        return authorize_googlephotos_function()
+    gphotos = build('photoslibrary', 'v1', credentials=google.oauth2.credentials.Credentials(config['google-photos']['token'], refresh_token=config['google-photos']['refresh_token'], token_uri=config['google-photos']['token_uri'], client_id=config['google-photos']['client_id'], client_secret=config['google-photos']['client_secret']))
