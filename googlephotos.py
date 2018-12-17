be0d2a9f246f004fec47dc11cf206ca24eeb606d
Novalinium <nova@noblejury.com>
2018-12-17 16:28:27 -0500

    GPhotos debugging

--- googlephotos.py
+++ googlephotos.py
@@ -69,6 +69,7 @@ async def twilestia_function(message, client, args):
     try:
         image = random.choice(list(gphotos.mediaItems().search(body={"albumId":config['google-photos']['twilestia']}).execute().get("mediaItems")))
         fullSizeImage = "{}=w{}-h{}".format(image.get("baseUrl"), image.get("mediaMetadata").get("width"), image.get("mediaMetadata").get("height"))
+        print(fullSizeImage)
         async with aiohttp.ClientSession() as session:
             async with session.get(fullSizeImage) as resp:
                 buffer = io.BytesIO(await resp.read())
