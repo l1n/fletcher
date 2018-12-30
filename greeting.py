34dd266d4d836a78a329cb6dfb8e2776d5dedc29
Novalinium <nova@noblejury.com>
2018-12-30 04:20:49 -0500

    add_roles list to scalar context

--- greeting.py
+++ greeting.py
@@ -8,7 +8,7 @@ async def randomize_role_function(member, client, config):
         if type(member) is discord.Member:
             role = member.guild.get_role(int(random.choice(config['randomize_role_list'].split(','))))
             print("RRF: adding role "+str(role)+" to "+str(member))
-            await member.add_roles([role], reason='Per Randomize Role Guild setting (see randomize_role_list)', atomic=False)
+            await member.add_roles(role, reason='Per Randomize Role Guild setting (see randomize_role_list)', atomic=False)
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         print("RRF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
