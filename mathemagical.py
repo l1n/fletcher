7cfdc3b953ad8915e6f221caee30fa700944ae9f
Novalinium <nova@noblejury.com>
2019-01-27 17:36:50 -0500

    Swap from sympy to matplotlib for latex rendering

--- mathemagical.py
+++ mathemagical.py
@@ -4,16 +4,30 @@ import sympy
 from sys import exc_info
 import tempfile
 
+import matplotlib.pyplot as plt
+
+def renderLatex(formula, fontsize=12, dpi=300, format='svg', file=None):
+    """Renders LaTeX formula into image or prints to file.
+    """
+    fig = plt.figure(figsize=(0.01, 0.01))
+    fig.text(0, 0, u'${}$'.format(formula), fontsize=fontsize)
+
+    output = io.BytesIO() if file is None else file
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=MathTextWarning)
+        fig.savefig(output, dpi=dpi, transparent=True, format=format,
+                    bbox_inches='tight', pad_inches=0.0, frameon=False)
+
+    plt.close(fig)
+
+    if file is None:
+        output.seek(0)
+        return output
+
 async def latex_render_function(message, client, args):
     try:
         renderstring = "$$"+" ".join(args)+"$$"
-        preamble = r'\documentclass[14pt]{article}\usepackage{mathtools}\pagenumbering{gobble}\DeclareMathSizes{24}{40}{28}{20}\begin{document}'
-        # Unclear how to sandbox this well without a function whitelist :/
-        # See service file for sandboxing info
-        preview_file = io.BytesIO()
-        sympy.preview(renderstring, viewer="BytesIO", output="png", outputbuffer=preview_file, preamble=preamble, fontsize='14pt')
-        preview_file.seek(0)
-        await message.channel.send("`"+renderstring+"`", file=discord.File(preview_file, filename="fletcher-render.png"))
+        await message.channel.send("`"+renderstring+"`", file=discord.File(renderLatex(renderstring, format='png'), filename="fletcher-render.png"))
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         print("LRF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
