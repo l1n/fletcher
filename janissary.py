a41c086ae563ee8b52a30438e463b3ed631b7e5c
Novalinium <nova@noblejury.com>
2019-04-14 08:40:51 -0400

    PCF guild ID in DMs

--- janissary.py
+++ janissary.py
@@ -399,10 +399,14 @@ async def snooze_channel_function(message, client, args):
             channel = messagefuncs.xchannel(args[0].strip(), message.guild)
             if channel is None:
                 channel = message.channel
+        if message.guild is not None:
+            guild = message.guild
+        else:
+            guild = channel.guild
         global conn
         cur = conn.cursor()
         interval = "1 day"
-        cur.execute("INSERT INTO reminders (userid, guild, channel, message, content, scheduled, trigger_type) VALUES (%s, %s, %s, %s, %s, NOW() + INTERVAL '"+interval+"', 'unban');", [message.author.id, message.guild.id, message.channel.id, message.id, message.content])
+        cur.execute("INSERT INTO reminders (userid, guild, channel, message, content, scheduled, trigger_type) VALUES (%s, %s, %s, %s, %s, NOW() + INTERVAL '"+interval+"', 'unban');", [message.author.id, guild.id, message.channel.id, message.id, message.content])
         await channel.set_permissions(message.author, read_messages=False, read_message_history=False, send_messages=False, embed_links=False, reason="User requested snooze "+message.author.name)
         conn.commit()
         await message.add_reaction('✅')
