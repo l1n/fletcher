9c2814a8782f0a8235572c790b682df9c510903c
Novalinium <nova@noblejury.com>
2019-04-19 18:46:11 -0400

    Do a text comparison

--- text_manipulators.py
+++ text_manipulators.py
@@ -261,7 +261,7 @@ async def reaction_request_function(message, client, args):
             await target.add_reaction(emoji)
             await asyncio.sleep(1)
             try:
-                reaction, user = await client.wait_for('reaction_add', timeout=6000.0, check=lambda reaction, user: reaction.emoji == emoji)
+                reaction, user = await client.wait_for('reaction_add', timeout=6000.0, check=lambda reaction, user: str(reaction.emoji) == str(emoji))
             except asyncio.TimeoutError:
                 pass
             await target.remove_reaction(emoji, client.user)
