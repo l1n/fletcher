82b89bb15de84e57688f6bfe84166826bda8aa77
Novalinium <nova@noblejury.com>
2019-04-22 11:35:35 -0400

    Strip colons to allow more flexible xreact

--- text_manipulators.py
+++ text_manipulators.py
@@ -249,7 +249,8 @@ async def spoiler_function(message, client, args):
 
 async def reaction_request_function(message, client, args):
     try:
-        emoji = list(filter(lambda m: m.name == args[0], client.emojis))
+        emoji_query = args[0].strip(':')
+        emoji = list(filter(lambda m: m.name == emoji_query, client.emojis))
         if len(args) >= 2:
             emoji = emoji[int(args[1])]
         else:
