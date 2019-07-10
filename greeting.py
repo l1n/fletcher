4329f8eb2c86b3e819a349d91fc09c8c4e38e5c6
Novalinium <nova@noblejury.com>
2019-07-10 17:20:30 -0400

    Revert regex caching

--- greeting.py
+++ greeting.py
@@ -128,9 +128,8 @@ async def regex_filter(message, client, config):
             subject = str(message.author)
         else:
             subject = str(message.content)
-        if type(config['regex-pattern']) is not re.Pattern:
-            config['regex-pattern'] = re.compile(config['regex-pattern'])
-        matching = config['regex-pattern'].search(subject)
+        # Can't be cached in config since configparser requires string values
+        matching = re.search(config['regex-pattern'], subject)
         if matching and whitelist_mode:
             allowed = True
         elif not matching and whitelist_mode:
