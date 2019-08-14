6afa0f357fb9cc428ae174e4a643ada1c6435672
Novalinium <nova@noblejury.com>
2019-08-14 15:43:28 -0400

    Change up logging

--- danbooru.py
+++ danbooru.py
@@ -21,8 +21,7 @@ async def posts_search_function(message, client, args):
                 }
         async with session.get(f'{base_url}/posts.json', params=params) as resp:
             response_body = await resp.json()
-            logger.debug(resp.status)
-            logger.debug(response_body)
+            logger.debug(resp.url)
             async with session.get(response_body[0]['file_url']) as resp:
                 buffer = io.BytesIO(await resp.read())
                 if resp.status != 200:
