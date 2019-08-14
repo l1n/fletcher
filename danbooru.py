8250bf295a357d0b212f45ca9c57ea4e2d1fa963
Novalinium <nova@noblejury.com>
2019-08-14 15:51:37 -0400

    When no image found don't just crash

--- danbooru.py
+++ danbooru.py
@@ -22,6 +22,8 @@ async def posts_search_function(message, client, args):
         async with session.get(f'{base_url}/posts.json', params=params) as resp:
             response_body = await resp.json()
             logger.debug(resp.url)
+            if len(response_body) == 0:
+                return await message.channel.send('No images found for query')
             async with session.get(response_body[0]['file_url']) as resp:
                 buffer = io.BytesIO(await resp.read())
                 if resp.status != 200:
