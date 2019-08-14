8556706adc577e3aced1e7637f20b2e6364c94c1
Novalinium <nova@noblejury.com>
2019-08-14 15:26:11 -0400

    Fix logging

--- danbooru.py
+++ danbooru.py
@@ -3,6 +3,10 @@ import discord
 import io
 from sys import exc_info
 
+import logging
+logger = logging.getLogger('fletcher')
+
+
 session = None
 base_url = "https://testbooru.donmai.us"
 
@@ -16,6 +20,7 @@ async def posts_search_function(message, client, args):
                 }
         async with session.get(f'{base_url}/posts.json', params=params) as resp:
             response_body = await resp.json()
+            logging.debug(response_body)
             async with session.get(response_body[0]['file_url']) as resp:
                 buffer = io.BytesIO(await resp.read())
                 if resp.status != 200:
@@ -23,7 +28,7 @@ async def posts_search_function(message, client, args):
                 await message.channel.send(files=[discord.File(buffer, image.get("filename"))])
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
-        print(f"PSF[{exc_tb.tb_lineno}]: {type(e).__name__} {e}")
+        logger.error(f"PSF[{exc_tb.tb_lineno}]: {type(e).__name__} {e}")
 
 def autoload(ch):
     global config 
