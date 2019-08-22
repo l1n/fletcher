6f3c758bceb0d3660e480d7db01e31f94173dab8
Novalinium <nova@noblejury.com>
2019-08-21 23:02:30 -0400

    Caching for danbooru post search counts

--- danbooru.py
+++ danbooru.py
@@ -1,5 +1,6 @@
 import aiohttp
 from base64 import b64encode
+import cachetools
 import discord
 import io
 from sys import exc_info
@@ -27,34 +28,40 @@ async def posts_search_function(message, client, args):
         else:
             params['tags'] += ' rating:safe'
 
-        async with session.get(f'{base_url}/counts/posts.json', params=params) as resp:
+        post_count = await count_search_function(params)
+        if not post_count or post_count == 0:
+            return await message.channel.send('No images found for query')
+        params['random'] = 'true'
+        params['limit'] = 1
+        async with session.get(f'{base_url}/posts.json', params=params) as resp:
             response_body = await resp.json()
             logger.debug(resp.url)
+            logger.debug(response_body)
             if len(response_body) == 0:
                 return await message.channel.send('No images found for query')
-            post_count = response_body['counts']['posts']
-            if post_count == 0:
-                return await message.channel.send('No images found for query')
-            params['random'] = 'true'
-            params['limit'] = 1
-            async with session.get(f'{base_url}/posts.json', params=params) as resp:
-                response_body = await resp.json()
-                logger.debug(resp.url)
-                logger.debug(response_body)
-                if len(response_body) == 0:
-                    return await message.channel.send('No images found for query')
-                if response_body[0]['file_size'] > 8000000:
-                    url = response_body[0]['preview_file_url']
-                else:
-                    url = response_body[0]['file_url']
-                async with session.get(url) as resp:
-                    buffer = io.BytesIO(await resp.read())
-                    if resp.status != 200:
-                        raise Exception('HttpProcessingError: '+str(resp.status)+" Retrieving image failed!")
-                    await message.channel.send(f"{post_count} results\n<{base_url}/posts/?md5={response_body[0]['md5']}>", files=[discord.File(buffer, response_body[0]["md5"]+"."+response_body[0]["file_ext"])])
+            if response_body[0]['file_size'] > 8000000:
+                url = response_body[0]['preview_file_url']
+            else:
+                url = response_body[0]['file_url']
+            async with session.get(url) as resp:
+                buffer = io.BytesIO(await resp.read())
+                if resp.status != 200:
+                    raise Exception('HttpProcessingError: '+str(resp.status)+" Retrieving image failed!")
+                await message.channel.send(f"{post_count} results\n<{base_url}/posts/?md5={response_body[0]['md5']}>", files=[discord.File(buffer, response_body[0]["md5"]+"."+response_body[0]["file_ext"])])
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         logger.error(f"PSF[{exc_tb.tb_lineno}]: {type(e).__name__} {e}")
+
+@cachetools.func.ttl_cache
+async def count_search_function(params):
+    async with session.get(f'{base_url}/counts/posts.json', params=params) as resp:
+        response_body = await resp.json()
+        logger.debug(resp.url)
+        if len(response_body) == 0:
+            return None
+        post_count = response_body['counts']['posts']
+        return post_count
+
 
 def autoload(ch):
     global config 
