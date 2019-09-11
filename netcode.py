ba586325f8cdbeeaab174d37247c3090548484de
Novalinium <nova@noblejury.com>
2019-09-11 15:18:43 -0400

    netcode fetch edit

--- netcode.py
+++ netcode.py
@@ -1,13 +1,12 @@
 import io
 import aiohttp
 
-async def simple_get_image(url, session=None):
-    if not session:
-        session = aiohttp.ClientSession(headers={
-            'User-Agent': 'Fletcher/0.1 (operator@noblejury.com)'
-            })
-    async with session.get(url) as resp:
-        buffer = io.BytesIO(await resp.read())
-        if resp.status != 200:
-            raise Exception('HttpProcessingError: '+str(resp.status)+" Retrieving image failed!")
-        return buffer
+async def simple_get_image(url):
+    async with aiohttp.ClientSession(headers={
+        'User-Agent': 'Fletcher/0.1 (operator@noblejury.com)'
+        }) as session:
+        async with session.get(url) as resp:
+            buffer = io.BytesIO(await resp.read())
+            if resp.status != 200:
+                raise Exception('HttpProcessingError: '+str(resp.status)+" Retrieving image failed!")
+            return buffer
