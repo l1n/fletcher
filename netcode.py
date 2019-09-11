8dea96013e6129b2ef577610d7094fadac4b1653
Novalinium <nova@noblejury.com>
2019-09-11 15:25:26 -0400

    Coerce types

--- netcode.py
+++ netcode.py
@@ -10,7 +10,7 @@ async def simple_get_image(url):
         async with aiohttp.ClientSession(headers={
             'User-Agent': 'Fletcher/0.1 (operator@noblejury.com)'
             }) as session:
-            async with session.get(url) as resp:
+            async with session.get(str(url)) as resp:
                 buffer = io.BytesIO(await resp.read())
                 if resp.status != 200:
                     raise Exception('HttpProcessingError: '+str(resp.status)+" Retrieving image failed!")
