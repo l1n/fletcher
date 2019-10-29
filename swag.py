2a4172303aa59140d943f89ba4413e2bb98d8adb
Novalinium <nova@noblejury.com>
2019-10-29 12:37:59 -0400

    join_rank

--- swag.py
+++ swag.py
@@ -15,6 +15,8 @@ from functools import partial
 
 logger = logging.getLogger('fletcher')
 
+session = None
+
 uwu_responses = {
         'public': [
             '*blush* For me?',
@@ -51,46 +53,46 @@ async def uwu_function(message, client, args, responses=uwu_responses):
         logger.error("UWU[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
 
 async def retrowave_function(message, client, args):
+    global session
     try:
-        async with aiohttp.ClientSession() as session:
-            params = aiohttp.FormData()
-            params.add_field('bcg',random.randint(1, 5))
-            params.add_field('txt',random.randint(1, 4))
-            text_parts = message.content
-            text_parts = messagefuncs.sanitize_font.sub('', text_parts)
-            if '/' in text_parts:
-                if len(args) == 3 and type(args[1]) is discord.Member:
-                    pass
-                else:
-                    text_parts = text_parts[10:].strip()
-                text_parts = [part.strip() for part in text_parts.split('/')]
-                if len(text_parts) == 0:
-                    text_parts = ['', '', '']
-                elif len(text_parts) == 1:
-                    text_parts = ['', text_parts[0], '']
-                elif len(text_parts) == 2:
-                    text_parts += ['']
-            else:
-                text_parts = text_parts.split()
-                if len(args) == 3 and type(args[1]) is discord.Member:
-                    pass
-                else:
-                    text_parts = text_parts[1:]
-                part_len = int(len(text_parts)/3)
-                if part_len > 1:
-                    text_parts = [" ".join(text_parts[:part_len]), " ".join(text_parts[part_len:2*part_len]), " ".join(text_parts[2*part_len:])]
-                else:
-                    text_parts = [" ".join(text_parts[0:1]), " ".join(text_parts[1:2]), " ".join(text_parts[2:])]
-            params.add_field('text1',text_parts[0])
-            params.add_field('text2',text_parts[1])
-            params.add_field('text3',text_parts[2])
-            logger.debug("RWF: "+str(text_parts))
-            async with session.post('https://m.photofunia.com/categories/all_effects/retro-wave?server=2', data=params) as resp:
-                request_body = (await resp.read()).decode('UTF-8')
-                root = html.document_fromstring(request_body)
-                async with session.get(root.xpath('//a[@class="download-button"]')[0].attrib['href']) as resp:
-                    buffer = io.BytesIO(await resp.read())
-                return await message.channel.send(files=[discord.File(buffer, 'retrowave.jpg')])
+       params = aiohttp.FormData()
+       params.add_field('bcg',random.randint(1, 5))
+       params.add_field('txt',random.randint(1, 4))
+       text_parts = message.content
+       text_parts = messagefuncs.sanitize_font.sub('', text_parts)
+       if '/' in text_parts:
+           if len(args) == 3 and type(args[1]) is discord.Member:
+               pass
+           else:
+               text_parts = text_parts[10:].strip()
+           text_parts = [part.strip() for part in text_parts.split('/')]
+           if len(text_parts) == 0:
+               text_parts = ['', '', '']
+           elif len(text_parts) == 1:
+               text_parts = ['', text_parts[0], '']
+           elif len(text_parts) == 2:
+               text_parts += ['']
+       else:
+           text_parts = text_parts.split()
+           if len(args) == 3 and type(args[1]) is discord.Member:
+               pass
+           else:
+               text_parts = text_parts[1:]
+           part_len = int(len(text_parts)/3)
+           if part_len > 1:
+               text_parts = [" ".join(text_parts[:part_len]), " ".join(text_parts[part_len:2*part_len]), " ".join(text_parts[2*part_len:])]
+           else:
+               text_parts = [" ".join(text_parts[0:1]), " ".join(text_parts[1:2]), " ".join(text_parts[2:])]
+       params.add_field('text1',text_parts[0])
+       params.add_field('text2',text_parts[1])
+       params.add_field('text3',text_parts[2])
+       logger.debug("RWF: "+str(text_parts))
+       async with session.post('https://m.photofunia.com/categories/all_effects/retro-wave?server=2', data=params) as resp:
+           request_body = (await resp.read()).decode('UTF-8')
+           root = html.document_fromstring(request_body)
+           async with session.get(root.xpath('//a[@class="download-button"]')[0].attrib['href']) as resp:
+               buffer = io.BytesIO(await resp.read())
+           return await message.channel.send(files=[discord.File(buffer, 'retrowave.jpg')])
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         logger.error("RWF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
@@ -118,25 +120,24 @@ async def shindan_function(message, client, args):
             else:
                 await message.channel.send('Please specify a name-based shindan to use from https://en.shindanmaker.com/')
                 return
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    request_body = (await resp.read()).decode('UTF-8')
-                    root = html.document_fromstring(request_body)
-                    author = ""
-                    if root.xpath('//span[@class="a author_link"]'):
-                        author = " by "+root.xpath('//span[@class="a author_link"]')[0].text_content().strip()
-                    embedPreview = discord.Embed(
-                            title=root.xpath('//div[@class="shindantitle2"]')[0].text_content().strip(),
-                            description=root.xpath('//div[@class="shindandescription"]')[0].text_content().strip(),
-                            url=url
-                            ).set_footer(
-                                    icon_url=message.author.avatar_url,
-                                    text="ShindanMaker {} on behalf of {}".format(
-                                        author,
-                                        message.author.display_name
-                                        ))
-                    resp = await message.channel.send(embed=embedPreview)
-                    await resp.add_reaction('📛')
+            async with session.get(url) as resp:
+                request_body = (await resp.read()).decode('UTF-8')
+                root = html.document_fromstring(request_body)
+                author = ""
+                if root.xpath('//span[@class="a author_link"]'):
+                    author = " by "+root.xpath('//span[@class="a author_link"]')[0].text_content().strip()
+                embedPreview = discord.Embed(
+                        title=root.xpath('//div[@class="shindantitle2"]')[0].text_content().strip(),
+                        description=root.xpath('//div[@class="shindandescription"]')[0].text_content().strip(),
+                        url=url
+                        ).set_footer(
+                                icon_url=message.author.avatar_url,
+                                text="ShindanMaker {} on behalf of {}".format(
+                                    author,
+                                    message.author.display_name
+                                    ))
+                resp = await message.channel.send(embed=embedPreview)
+                await resp.add_reaction('📛')
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         logger.error("SDF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
@@ -306,10 +307,9 @@ async def scp_function(message, client, args):
             if '-' in message.content:
                 args.append(message.content.split('-', 1)[1].strip())
             else:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get('http://www.scp-wiki.net/random:random-scp') as resp:
-                        request_body = (await resp.read()).decode('UTF-8')
-                        args.append(request_body.split('iframe-redirect#')[1].split('"')[0].split('-')[2])
+                async with session.get('http://www.scp-wiki.net/random:random-scp') as resp:
+                    request_body = (await resp.read()).decode('UTF-8')
+                    args.append(request_body.split('iframe-redirect#')[1].split('"')[0].split('-')[2])
         if args[0][0].isdigit():
              url = "http://www.scp-wiki.net/scp-"+args[0]
         elif args[0].startswith("http://www.scp-wiki.net/"):
@@ -317,42 +317,41 @@ async def scp_function(message, client, args):
         else:
             await message.channel.send('Please specify a SCP number from http://www.scp-wiki.net/')
             return
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                request_body = (await resp.read()).decode('UTF-8')
-                root = html.document_fromstring(request_body)
-                author = ""
-                title = root.xpath('//div[@id="page-title"]')[0].text_content().strip()
-                content = root.xpath('//div[@id="page-content"]/p[strong]')
-                add_fields = True
-                try:
-                    for i in range(0, 4):
-                        content[i][0].drop_tree()
-                    description = str(markdownify(etree.tostring(content[3]).decode()[3:-5].strip())[:2000])
-                except IndexError as e:
-                    logger.debug(f'SCP: {e}')
-                    add_fields = False
-                    description = str(markdownify(etree.tostring(root.xpath('//div[@id="page-content"]')[0]).decode()))[:2000].strip()
-                    if not description:
-                        description = root.xpath('//div[@id="page-content"]').text_content()[:2000].strip()
-                embedPreview = discord.Embed(
-                        title=title,
-                        description=description,
-                        url=url
-                        )
-                embedPreview.set_footer(
-                        icon_url='http://download.nova.anticlack.com/fletcher/scp.png',
-                        text=f'On behalf of {message.author.display_name}'
-                        )
-                if root.xpath('//div[@class="scp-image-block block-right"]'):
-                    embedPreview.set_thumbnail(url=root.xpath('//div[@class="scp-image-block block-right"]//img')[0].attrib['src'])
-                if add_fields:
-                    embedPreview.add_field(name='Object Class', value=str(markdownify(etree.tostring(content[1]).decode()[3:-5].strip()))[:2000], inline=True)
-                    scp = str(markdownify(etree.tostring(content[2]).decode()[3:-5].strip()))[:2000]
-                    if scp:
-                        embedPreview.add_field(name='Special Containment Procedures', value=scp, inline=False)
-                embedPreview.add_field(name='Tags', value=', '.join([node.text_content().strip() for node in root.xpath('//div[@class="page-tags"]/span/a')]), inline=True)
-                resp = await message.channel.send(embed=embedPreview)
+        async with session.get(url) as resp:
+            request_body = (await resp.read()).decode('UTF-8')
+            root = html.document_fromstring(request_body)
+            author = ""
+            title = root.xpath('//div[@id="page-title"]')[0].text_content().strip()
+            content = root.xpath('//div[@id="page-content"]/p[strong]')
+            add_fields = True
+            try:
+                for i in range(0, 4):
+                    content[i][0].drop_tree()
+                description = str(markdownify(etree.tostring(content[3]).decode()[3:-5].strip())[:2000])
+            except IndexError as e:
+                logger.debug(f'SCP: {e}')
+                add_fields = False
+                description = str(markdownify(etree.tostring(root.xpath('//div[@id="page-content"]')[0]).decode()))[:2000].strip()
+                if not description:
+                    description = root.xpath('//div[@id="page-content"]').text_content()[:2000].strip()
+            embedPreview = discord.Embed(
+                    title=title,
+                    description=description,
+                    url=url
+                    )
+            embedPreview.set_footer(
+                    icon_url='http://download.nova.anticlack.com/fletcher/scp.png',
+                    text=f'On behalf of {message.author.display_name}'
+                    )
+            if root.xpath('//div[@class="scp-image-block block-right"]'):
+                embedPreview.set_thumbnail(url=root.xpath('//div[@class="scp-image-block block-right"]//img')[0].attrib['src'])
+            if add_fields:
+                embedPreview.add_field(name='Object Class', value=str(markdownify(etree.tostring(content[1]).decode()[3:-5].strip()))[:2000], inline=True)
+                scp = str(markdownify(etree.tostring(content[2]).decode()[3:-5].strip()))[:2000]
+                if scp:
+                    embedPreview.add_field(name='Special Containment Procedures', value=scp, inline=False)
+            embedPreview.add_field(name='Tags', value=', '.join([node.text_content().strip() for node in root.xpath('//div[@class="page-tags"]/span/a')]), inline=True)
+            resp = await message.channel.send(embed=embedPreview)
     except Exception as e:
         exc_type, exc_obj, exc_tb = exc_info()
         logger.error("SCP[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
@@ -427,7 +426,19 @@ async def qdb_search_function(message, client, args):
         logger.error("QSF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
 
 
+async def join_rank_function(message, client, args):
+    try:
+        member_rank = sorted(message.guild.members, key=lambda member: member.joined_at).index(message.author)+1
+        # Gareth on codegolf
+        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+        await messagefuncs.sendWrappedMessage(f'{message.author.mention} is the {ordinal(member_rank)} member to join this server', message.channel)
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = exc_info()
+        logger.error("JRF[{}]: {} {}".format(exc_tb.tb_lineno, type(e).__name__, e))
+
+
 def autoload(ch):
+    global session
     ch.add_command({
         'trigger': ['!uwu', '<:uwu:445116031204196352>', '<:uwu:269988618909515777>', '<a:rainbowo:493599733571649536>', '<:owo:487739798241542183>', '<:owo:495014441457549312>', '<a:OwO:508311820411338782>', '!good', '!aww'],
         'function': uwu_function,
@@ -537,4 +548,18 @@ def autoload(ch):
         'long_run': False,
         'args_name': [],
         'description': 'Fling sparkles!'
+        })
+    ch.add_command({
+        'trigger': ['!join_rank'],
+        'function': join_rank_function,
+        'async': False,
+        'args_num': 0,
+        'long_run': False,
+        'args_name': [],
+        'description': 'Check what number member you were to join this server.'
+        })
+    if session:
+        session.close()
+    session = aiohttp.ClientSession(headers={
+        'User-Agent': 'Fletcher/0.1 (operator@noblejury.com)',
         })
