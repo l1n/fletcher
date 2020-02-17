cd6cd28d31716bd631c839b96b125a536737a4cb
Novalinium <nova@noblejury.com>
2020-02-16 21:21:28 -0500

    Better URLs

--- swag.py
+++ swag.py
@@ -627,7 +627,8 @@ async def lifx_function(message, client, args):
                  return await message.channel.send(f"LIFX Error: {request_body['error']} (data sent was `{data}`, selector was `selector`")
                  await message.add_reaction("🚫")
             embedPreview = discord.Embed(title=f"Updated Lights: {data}")
-            embedPreview.set_image(url=f"https://novalinium.com/rationality/lifx-color.pl?string={data.replace(' ', '%20')}&ext=png")
+            dataStr = f"{data}"
+            embedPreview.set_image(url=f"https://novalinium.com/rationality/lifx-color.pl?string={dataStr}&ext=png")
             embedPreview.set_footer(
                 icon_url="http://download.nova.anticlack.com/fletcher/favicon_lifx_32x32.png",
                 text=f"On behalf of {message.author.display_name}",
