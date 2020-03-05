597a93ab5613b4675c004a19bf8716969d3ab108
Novalinium <nova@noblejury.com>
2020-03-05 15:09:28 -0500

    Support guild and channel default timezones

--- chronos.py
+++ chronos.py
@@ -41,6 +41,10 @@ def get_tz(message=None, user=None, guild=None):
         guild = guild.id
     if ch.user_config(user, guild, 'tz'):
         tz = pytz.timezone(ch.user_config(message.author.id, message.guild.id, 'tz'))
+    elif ch.scoped_config(guild=guild, channel=message.channel).get('tz'):
+        tz = pytz.timezone(ch.scoped_config(guild=guild, channel=message.channel).get('tz'))
+    elif ch.scoped_config(guild=guild).get('tz'):
+        tz = pytz.timezone(ch.scoped_config(guild=guild).get('tz'))
     else:
         tz = pytz.utc
     return tz
