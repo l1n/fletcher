2336f8d905ceeedd9d23927fe7bec269511cfb2e
Novalinium <nova@noblejury.com>
2020-05-20 14:18:07 -0400

    Voice timeout

--- main.py
+++ main.py
@@ -978,6 +978,9 @@ async def doissetep_omega_autoconnect():
             # Reset canticum_message when reloaded [workaround for https://todo.sr.ht/~nova/fletcher/6]
             canticum_message = None
             return doissetep_omega
+        except asyncio.exceptions.TimeoutError as e:
+            logger.debug('Omega timeout')
+            pass
         except AttributeError as e:
             logger.exception(e)
 
