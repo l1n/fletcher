2c641a43690a4ad7f3f457ac6e9e93e7b77bc02e
Novalinium <nova@noblejury.com>
2020-08-10 18:49:30 -0400

    NoneType also caught

--- janissary.py
+++ janissary.py
@@ -1250,7 +1250,7 @@ async def add_inbound_sync_function(message, client, args):
         soon = client.get_emoji(664472443053932604)
         try:
             await message.add_reaction(soon)
-        except discord.Forbidden:
+        except (discord.Forbidden, InvalidArgument):
             soon = "🔜"
             await message.add_reaction(soon)
         await message.channel.create_webhook(
